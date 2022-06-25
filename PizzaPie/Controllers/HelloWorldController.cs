7813928713f4c3d9a69084e979b06bb41d183c0c
Shaoli Lu <shaoli_lu@yahoo.co>
2022-06-24 22:35:18 -0500

    dogo

---
+++ PizzaPie/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PizzaPie.Models;
+
+namespace PizzaPie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            DogViewModel dogo = new DogViewModel()
+            {
+                Name ="Beso", Age = 2
+            };
+            return View(dogo);
+        }
+
+        public string Hello()
+        {
+            return "Who's there?";
+        }
+    }
+}
